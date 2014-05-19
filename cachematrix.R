f157da2337a657cda7c5d16c350bd153ee3465d1
YL <yuwei2341@hotmail.com>
2014-05-18 23:22:17 -0700

    test

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+# makeCacheMatrix() creates a special "matrix", which is
+# really a list containing a function to
+# 1.  set the value of the matrix
+# 2.  get the value of the matrix
+# 3.  set the value of the inverse
+# 4.  get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invX <- NULL
+    set <- function(y) {
+        x <<- y
+        invX <<- NULL
+    }
+    get <- function() x
+    setInv <- function(inv) invX <<- inv
+    getInv <- function() invX
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+
+# cacheSolve(x) calculates the inverse of the special "matrix" x, 
+# created with the above function. However, it first checks to see if the
+# inverse has already been calculated. If so, it `get`s the inverse from the
+# cache and skips the computation. Otherwise, it calculates the inverse of
+# the data and sets the value of the inverse in the cache via the `setInv`
+# function.
+# Before computing the inverse, the function determines if x is a square matrix
+# and return NULL if not; If x is square, then the function determines if x 
+# is non-singular and return NULL if not. The latter needs the package matrixcalc.
+# In case one doesn't install it, that part is commented
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    invX <- x$getInv()
+    if(!is.null(invX)) {
+        message("getting cached data")
+        return(invX)
+    }
+    data <- x$get()
+    
+    ## make sure it's square
+    if (ncol(data) != nrow(data)) {
+        message("The matrix must be square")
+        return()
+    }
+    
+    ## make sure the squre matrix is non-singular; need the package matrixcalc
+#     if !is.non.singular.matrix(data) {
+#         message("The square matrix must be non-singular")
+#         return()
+#     }
+    
+    invX <- solve(data, ...)
+    x$setInv(invX)
+    invX
 }
